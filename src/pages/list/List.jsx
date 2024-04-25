a1596daa5ace731bed4763090733349b4c752ee1
AliDlt <alidlt1@yahoo.com>
2024-04-25 15:59:34 +0330

    list state

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,4 +1,3 @@
-// list.js
 import React from "react";
 import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
@@ -9,6 +8,11 @@ import { useLocation } from "react-router-dom";
 
 const List = () => {
   const { state } = useLocation();
+
+  // Check if state exists and has hotels array
+  if (!state || !state[0] || !state[0].hotels) {
+    return null; // or render some default component or message
+  }
 
   return (
     <div>
